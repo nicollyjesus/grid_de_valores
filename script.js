6de87b0f1a533c8e95b656a77f81034f4262caf6
nicolly jesus <nicolly.jesus.nj@gmail.com>
2023-06-12 21:45:05 -0300

    meu primeiro commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// Função de exemplo para exibir uma mensagem de boas-vindas
+function exibirMensagem() {
+    alert('Welcome!');
+  }
+  
+  // Event listener para chamar a função quando a página for totalmente carregada
+  document.addEventListener('DOMContentLoaded', function() {
+    exibirMensagem();
+  });
+
+  
